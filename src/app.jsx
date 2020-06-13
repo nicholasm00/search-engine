9d3e6232cd699f30a600359d17d292688762b874
justinma98 <justinma3@gmail.com>
2020-06-13 17:40:27 -0500

    added basic search

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import './app.scss';
+import React, { useState } from 'react';
 import Settings from './components/settings/Settings';
 import SearchBar from './components/searchbar/SearchBar';
+import { data } from './data';
+import './app.scss';
 
 export default function App() {
+  const [currSearch, setCurrSearch] = useState(data[0]);
+
   return (
     <div className="app">
       <div className="app__corner -topRight">
         <Settings />
       </div>
       <div className="app__dashboard">
-        <SearchBar />
+        <SearchBar searchData={currSearch} />
       </div>
     </div>
   );
